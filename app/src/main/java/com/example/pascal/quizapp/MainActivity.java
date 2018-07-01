418bb2fa461506553f7a662e167af386c52871ea
Alain Pascal <ialainpascal@gmail.com>
2018-07-01 19:29:15 +0300

    Code for Question 1 in MainActivity.java

--- app/src/main/java/com/example/pascal/quizapp/MainActivity.java
+++ app/src/main/java/com/example/pascal/quizapp/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.pascal.quizapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score = 0;
+    String answerOne = "Andy Rubin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the 'submit' button for Question 1 is clicked
+     */
+    public void submitQuestionOne(View view) {
+        EditText answerOneTextField = findViewById(R.id.edit_text_q1);
+        String userAnswerOne = answerOneTextField.getText().toString();
+
+        // Comparing the user's answer with the correct answer
+        if (userAnswerOne.equals(answerOne)) {
+            // If the answer is correct then we add 1 to the user's score
+            score += 1;
+
+            // This will display a toast message saying that the answer is correct and display the score
+            Toast.makeText(this, "Correct! You scored: " + score + " points out of 5.", Toast.LENGTH_SHORT).show();
+        } else {
+            // This will display a toast message saying that the answer is incorrect and display the score
+            Toast.makeText(this, "Sorry, you got it wrong. You scored: " + score + " points out of 5.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * This method is called when the 'view answer' button  for Question 1 is clicked
+     */
+    public void viewAnswerOne(View view) {
+        // This will display a toast message with the correct answer
+        Toast.makeText(this, "The founder of Android is " + answerOne + ".", Toast.LENGTH_SHORT).show();
     }
 }
