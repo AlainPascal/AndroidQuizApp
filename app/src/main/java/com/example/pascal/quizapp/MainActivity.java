fe76ab964df099c10fb2affccce23d26aaf8410b
Alain Pascal <ialainpascal@gmail.com>
2018-07-01 21:49:20 +0300

    Code for Question 2 in MainActivity.java

--- app/src/main/java/com/example/pascal/quizapp/MainActivity.java
+++ app/src/main/java/com/example/pascal/quizapp/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
     String answerOne = "Andy Rubin";
+    String answerTwo = "False";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,4 +46,41 @@ public class MainActivity extends AppCompatActivity {
         // This will display a toast message with the correct answer
         Toast.makeText(this, "The founder of Android is " + answerOne + ".", Toast.LENGTH_SHORT).show();
     }
+
+    /**
+     * This method is called when the 'submit' button for Question 2 is clicked
+     */
+    public void submitQuestionTwo(View view) {
+        String userAnswerTwo = "";
+
+        RadioButton radioButtonFalse = findViewById(R.id.radio_button_false);
+        boolean radioFalse = radioButtonFalse.isChecked();
+        if (radioFalse) {
+            userAnswerTwo = "False";
+        }
+
+        RadioButton radioButtonTrue = findViewById(R.id.radio_button_true);
+        boolean radioTrue = radioButtonTrue.isChecked();
+        if (radioTrue) {
+            userAnswerTwo = "True";
+        }
+
+        if (userAnswerTwo.equals(answerTwo)) {
+            score += 1;
+
+            Toast.makeText(this, "Correct! You scored: " + score + " points out of 5.", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Sorry, you got it wrong. You scored: " + score + " points out of 5.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * This method is called when the 'view answer' button  for Question 2 is clicked
+     */
+    public void viewAnswerTwo(View view) {
+        // This will display a toast message with the correct answer
+        Toast.makeText(this, answerTwo + ". Android Apps can also be developed in other programming languages such C/C++, Kotlin.", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
